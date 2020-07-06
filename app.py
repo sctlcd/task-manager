fa38cc1cca9a36555ba98a920f00b2b49077ca92
8 <lucinda.souchet@outlook.com>
2020-07-06 01:55:05 +0100

    connect Flask to MongoDB Atlas

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
-
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = ''
 
 @app.route('/')
 def hello():
